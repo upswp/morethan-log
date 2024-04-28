0dfa8c03a39198dda355e5204cc68952f84baaed
Samuel.Park <samuel@floatic.io>
2024-04-29 00:14:11 +0900

    docs : setting information

--- site.config.js
+++ site.config.js
@@ -1,14 +1,14 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "samuel.park",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
+    role: "backend developer",
     bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
-    instagram: "",
+    email: "tkddnsos@gmail.com",
+    linkedin: "Sangwoo (Samuel) Park",
+    github: "upswp",
+    instagram: "upsw.p",
   },
   projects: [
     {
@@ -18,8 +18,9 @@ const CONFIG = {
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "개발여정기",
+    description: "개발 성장기에 대한 기록입니다.",
+    theme: "auto"
   },
 
   // CONFIG configration (required)
